a48ca1f4ed0f160c4acbf10e1d74311d8bcef9de
Muhammad Rizal <rizal@ruangguru.com>
2020-08-29 16:50:03 +0700

    feat: remove thirtparty

--- server/renderer/html-template.js
+++ server/renderer/html-template.js
@@ -2,7 +2,6 @@ import { createLinkTag, createPreloadTag, createScriptTag, preconnectTags } from
 import { isProd } from '@utils';
 import { normalizeCSS } from '@utils/html/critical-css';
 import { preloadedMainStyles, preloadedMainScripts } from './get-main-assets';
-import { hotjarScript } from './thirdparty';
 
 export const getHeader = ({
   chunkExtractor,
@@ -57,7 +56,6 @@ export const getFooter = ({ chunkExtractor, initialState = {}, ssr = false, data
         window.__cache=${JSON.stringify(dataClient?.cache)};
         window.ENV="${process.env.ENV}";
 			</script>
-			${hotjarScript}
       ${chunkExtractor?.getScriptTags({ crossorigin: 'anonymous' })?.replace(/\<script async/g, '<script defer') ||
         preloadedMainScripts(createScriptTag)}
     </body>
--- utils/html/preconnect-tags.js
+++ utils/html/preconnect-tags.js
@@ -1,12 +1,6 @@
 import createPreconnectTag from './create-preconnect-tag';
 
-const preconnectSources = [
-  'https://googleads.g.doubleclick.net',
-  'https://www.google-analytics.com',
-  'https://www.googleadservices.com',
-  'https://www.googletagmanager.com',
-  'https://connect.facebook.net',
-];
+const preconnectSources = ['https://res.cloudinary.com'];
 
 const preconnectTags = preconnectSources.map(createPreconnectTag).join('\n');
 
